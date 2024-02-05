cbf102f57ab303a06713df28ebcb703cdc4804fd
Andredev-dias <andredias.curitiba@gmail.com>
2024-02-05 12:00:40 -0300

    soma done

--- Activity01/script.js
+++ Activity01/script.js
@@ -1,8 +1,12 @@
 function somar() {
     var num1 = document.getElementById("num1").valueAsNumber;
     var num2 = document.getElementById("num2").valueAsNumber;
-    console.log(num1, "este é o primeiro numero")
-    console.log(num2, "este é o segundo numero")
+    var res = num1 + num2;
+    document.getElementById("resultadoSoma").textContent = res.toFixed(2);
+}
+function sub(){
+  
+
 }
 
 
