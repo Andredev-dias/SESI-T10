a25414f6d2b7f5513fa9205b02cfacbec5414dbf
Andredev-dias <andredias.curitiba@gmail.com>
2024-02-05 09:54:53 -0300

    console logs

---
+++ Activity01/script.js
@@ -0,0 +1,9 @@
+function somar() {
+    var num1 = document.getElementById("num1").valueAsNumber;
+    var num2 = document.getElementById("num2").valueAsNumber;
+    console.log(num1, "este é o primeiro numero")
+    console.log(num2, "este é o segundo numero")
+}
+
+
+
