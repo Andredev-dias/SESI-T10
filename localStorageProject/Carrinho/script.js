7dcf085e435355f84955b826d78f7f1d18e447e6
Andredev-dias <andredias.curitiba@gmail.com>
2024-03-25 09:37:52 -0300

    opa

--- localStorageProject/Carrinho/script.js
+++ localStorageProject/Carrinho/script.js
@@ -35,4 +35,40 @@ function removeItemDoCarrinho(index){
     exibirCarrinho();
 });
 
+function gerarDocumentoWord(){
+    const listaElement = document.getElementById("lista")
+    const totalElement = document.getElementById("total")
+
+    const listaClone = listaElement.cloneNode(true);
+
+    $(listaClone).find("button").remove();
+
+    const listaHtml = listaClone.innerHTML;
+    const totalHtml = totalElement.innerHTML;
+
+    const conteudoHtml = `
+        <html>
+            <head>
+                <meta charset="UTF-8"/>
+            </head>
+            <body>
+                <h1>Pedido confirmado</h1>
+                <h3>Agradecemos sua preferênia</h3>
+                <br>
+                ${listaHtml}
+                <br>
+                <br>
+                ${totalHtml}
+            </body>
+        </html>
+    `;
+    const blob = new Blob([conteudoHtml], {type: "application/msword"});
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(blob);
+    link.download = "carrinho.doc";
+    link.click();
+    document.getElementById("pedido").style.display = "block";
+
+}
+
 const successClose = () => document.getElementById("pedido").style.display = "none";
